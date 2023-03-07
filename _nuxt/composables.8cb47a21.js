8ab91f11e73c7a16f2b07ea020c8e9a42b138cb9
Ayumi Takahashi <ayumee528@gmail.com>
2023-03-07 07:01:17 -0800

    Updates

--- _nuxt/composables.8cb47a21.js
+++
@@ -1,1 +0,0 @@
-import{Q as r}from"./entry.8081fbb6.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
---
+++ _nuxt/startale-horizontal.ddb7fe6c.js
@@ -0,0 +1,1 @@
+import"./entry.a03cb34c.js";const r=""+new URL("startale-horizontal.82c1dbcd.svg",import.meta.url).href;export{r as _};
